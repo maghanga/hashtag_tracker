2b43169be140c0c490548adc59d42de8ba5dbe8f
thinknehru <thinknehru@gmail.com>
2020-01-31 16:14:44 +0300

    Set an onClickListener for the button to navigate to the Search hashtag activity

--- app/src/main/java/com/thinknehru/hashtag_tracker/MainActivity.java
+++ app/src/main/java/com/thinknehru/hashtag_tracker/MainActivity.java
@@ -2,7 +2,9 @@ package com.thinknehru.hashtag_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -21,5 +23,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mNavigateToSearchButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SearchHashtagActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
