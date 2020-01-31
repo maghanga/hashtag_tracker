6e83495244fec7516ea3afc8a281990eeefdb2ab
thinknehru <thinknehru@gmail.com>
2020-01-31 15:51:59 +0300

    Add a textview with instructions to the search hashtags activity

---
+++ app/src/main/java/com/thinknehru/hashtag_tracker/SearchHashtagActivity.java
@@ -0,0 +1,14 @@
+package com.thinknehru.hashtag_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchHashtagActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_hashtag);
+    }
+}
