0800f3f23ce10d401cfc1ca591e4d59bf4c8d7fe
thinknehru <thinknehru@gmail.com>
2020-01-31 16:11:02 +0300

    Integrate ButterKnife into the project and bind the views into the main activity

--- app/src/main/java/com/thinknehru/hashtag_tracker/MainActivity.java
+++ app/src/main/java/com/thinknehru/hashtag_tracker/MainActivity.java
@@ -3,13 +3,23 @@ package com.thinknehru.hashtag_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    
+    @BindView(R.id.navigateToSearchButton)
+    Button mNavigateToSearchButton;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
