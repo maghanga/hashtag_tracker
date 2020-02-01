cbc062005076aab03fc3b69e13b6e29a9ddce9a5
thinknehru <thinknehru@gmail.com>
2020-02-01 10:29:15 +0300

    Bind the views using butterknife in the Search hashtags activity

--- app/src/main/java/com/thinknehru/hashtag_tracker/HahstagsActivity.java
+++ app/src/main/java/com/thinknehru/hashtag_tracker/HahstagsActivity.java
@@ -3,12 +3,32 @@ package com.thinknehru.hashtag_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class HahstagsActivity extends AppCompatActivity {
+    @BindView(R.id.searchHashtagButton)
+    Button mSearchHashtagButton;
+    @BindView(R.id.hashtagEditText)
+    EditText mHashtagEditText;
+    @BindView(R.id.instructionsTextView)
+    TextView mInstructionsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hahstags);
+        ButterKnife.bind(this);
+
+        mSearchHashtagButton.setOnClickListener(new View.OnClickListener(){
+
+        };
+
     }
 }
--- app/src/main/java/com/thinknehru/hashtag_tracker/SearchHashtagActivity.java
+++ app/src/main/java/com/thinknehru/hashtag_tracker/SearchHashtagActivity.java
@@ -3,8 +3,19 @@ package com.thinknehru.hashtag_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
 
 public class SearchHashtagActivity extends AppCompatActivity {
+    @BindView(R.id.searchHashtagButton)
+    Button mFindRestaurantsButton;
+    @BindView(R.id.hashtagEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
