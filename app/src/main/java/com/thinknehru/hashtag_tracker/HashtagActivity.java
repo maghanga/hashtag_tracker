0b4a6120474e93a0854e73d29a867b9dcd374ac0
thinknehru <thinknehru@gmail.com>
2020-02-01 13:36:18 +0300

    Create an ArrayAdapter and set our ListView adapter to the new adapter in the hashtag activity

--- app/src/main/java/com/thinknehru/hashtag_tracker/HashtagActivity.java
+++ app/src/main/java/com/thinknehru/hashtag_tracker/HashtagActivity.java
@@ -25,5 +25,7 @@ public class HashtagActivity extends AppCompatActivity {
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, posts);
         mListView.setAdapter(adapter);
 
+
+
     }
 }
