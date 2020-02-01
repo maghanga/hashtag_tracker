ce2ac27bb8a1f3312cae84ca0a3c40ab3f001b0a
thinknehru <thinknehru@gmail.com>
2020-02-01 10:50:07 +0300

    Gather hashtag data from the hashtag edittextview via an intent extra to pass it to hashtag activity

--- app/src/main/java/com/thinknehru/hashtag_tracker/SearchHashtagActivity.java
+++ app/src/main/java/com/thinknehru/hashtag_tracker/SearchHashtagActivity.java
@@ -2,24 +2,40 @@ package com.thinknehru.hashtag_tracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class SearchHashtagActivity extends AppCompatActivity {
     @BindView(R.id.searchHashtagButton)
-    Button mFindRestaurantsButton;
+    Button mSearchHashtagButton;
     @BindView(R.id.hashtagEditText)
-    EditText mLocationEditText;
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
+    EditText mHashtagEditText;
+    @BindView(R.id.instructionsTextView)
+    TextView mInstructionsTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_hashtag);
+        ButterKnife.bind(this);
+
+        mSearchHashtagButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String hashtag = mHashtagEditText.getText().toString();
+                Intent intent = new Intent(SearchHashtagActivity.this, HashtagActivity.class);
+                intent.putExtra("hashtag", hashtag);
+                startActivity(intent);
+            }
+        });
+
     }
 }
